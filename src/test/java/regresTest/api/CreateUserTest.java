39c546eeeeb65ca15b4caeae84103f0ddbcada13
darthviner666 <denvviner.work@gmail.com>
2025-04-24 20:36:26 +1000

    CreateUserTest fix

--- src/test/java/regresTest/api/CreateUserTest.java
+++ src/test/java/regresTest/api/CreateUserTest.java
@@ -2,6 +2,7 @@ package regresTest.api;
 
 import org.junit.jupiter.api.Test;
 import regresTest.api.endpoints.RegresEndpoints;
+import regresTest.api.pojo.rq.CreateUserRq;
 import regresTest.api.specs.RegresSpecs;
 
 import static io.restassured.RestAssured.given;
@@ -9,6 +10,12 @@ import static io.restassured.RestAssured.given;
 public class CreateUserTest {
     @Test
     public void CreateUserTest() {
+        CreateUserRq user = CreateUserRq
+                .builder()
+                .job("job")
+                .name("name")
+                .build();
+
         given()
                 .spec(RegresSpecs.requestSpecification())
                 .post(RegresEndpoints.CREATE_USER)
